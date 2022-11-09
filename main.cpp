4a6b3529de20999f69066a23ad15e6ce42d4c3a0
demidraksin <greatraksin@icloud.com>
2022-11-09 18:26:29 +0300

    Adding lab 6

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
+// #include <string>  <- если не работает string, напишите это
 using namespace std;
 
 int main() {
+    string phrase;
+    cout << "Введи фразу: ";
+    cin >> phrase;
+
+    cout << phrase << endl;
+    sort(phrase.begin(), phrase.end());
+    cout << phrase << endl;
 }
