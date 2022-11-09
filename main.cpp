87f15d0c927a8ef5c271e8ad9749322ed12f6d72
demidraksin <greatraksin@icloud.com>
2022-11-09 16:15:32 +0300

    Adding hw ptr

--- main.cpp
+++ main.cpp
@@ -1,24 +1,5 @@
 #include <iostream>
 using namespace std;
 
-double *give_memory(double *ptr_arr, int size) { // указатель на функцию
-    ptr_arr = new double[size];  // инициализировал динамический массив длиной size
-    return ptr_arr;  // передал массив из функции
-}
-
-void fill_array(double *ptr_arr, int size) {
-    // наполнить массив числами
-}
-
-void show_array(double *ptr_arr, int size) {
-    // вывести массив на экран
-}
-
-double *free_memory(double *ptr_array) {
-}
-
 int main() {
-    int length;
-    cout << "Insert array length: ";
-    cin >> length;
 }
