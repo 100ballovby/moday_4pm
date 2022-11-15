753c5d4eea62f5c7b1932f04440c9cc57aba4262
demidraksin <greatraksin@icloud.com>
2022-11-15 15:48:06 +0300

    Adding functions

--- main.cpp
+++ main.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
-// #include <string>  <- если не работает string, напишите это
 using namespace std;
 
-int main() {
-    string phrase;
-    cout << "Введи фразу: ";
-    cin >> phrase;
+int randint(int min, int max) {
+    int r = rand() % (max - min) + min;
+    return r;
+}
 
-    cout << phrase << endl;
-    sort(phrase.begin(), phrase.end());
-    cout << phrase << endl;
+
+int main() {
+    return 0;
 }
