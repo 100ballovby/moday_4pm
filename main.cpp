2196dc0c44e99ba8d7482bdb9406163028f56014
demidraksin <greatraksin@icloud.com>
2022-12-05 16:22:39 +0300

    Finishing struct lab

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 struct Student {
@@ -6,31 +7,35 @@ struct Student {
     string surname;
     string father_name;
     string address;
-    int group;
-    double rate;
+    string group;
+    string rate;
 };
 
-void show_data(Student *obj, int amount) {
-    cout << "№\t" << "ФИО\t\t\t\t" << "Адрес\t" << "Группа\t" << "Рейтинг\t" << endl;
+void create_array(Student *&obj, int amount) {
     for (int i = 0; i < amount; i++) {
-        cout << i << "\t" << obj[i].name << obj[i].surname << obj[i].father_name << "\t" << obj[i].address << "\t" << obj[i].group << "\t" << obj[i].rate << "\t" << endl;
+        cout << "Имя, Фамилия, Отчество, Адрес, Группа, Рейтинг студента ";
+        cin >> obj[i].name >> obj[i].surname >> obj[i].father_name >> obj[i].address >> obj[i].group >> obj[i].rate;
     }
 }
 
 int main() {
-    const int size = 3;
-    Student students[size];
+    int amount;
+    cout << "Сколько студентов? ";
+    cin >> amount;
+    auto *students = new Student[amount];
+    auto *buffer = new Student[amount];
+    create_array(students, amount);
 
-    for (int i = 0; i < size; i++) {
-        cout << "Имя, Фамилия, Отчество студента: ";
-        cin >> students[i].name >> students[i].surname >> students[i].father_name;
-        cout << "Адрес";
-        getline(cin, students[i].address);
-        cin.ignore(32767, '\n');
-        cout << "Группа: "; cin >> students[i].group;
-        cout << "Рейтинг: "; cin >> students[i].rate;
+    ofstream F1("../my_file.txt", ios::binary);
+    for (unsigned int i = 0; i < amount; i++) {
+        F1.write((char*)&students[i].name, sizeof(students[i].name));
+        F1.write((char*)&students[i].surname, sizeof(students[i].surname));
+        F1.write((char*)&students[i].father_name, sizeof(students[i].father_name));
+        F1.write((char*)&students[i].address, sizeof(students[i].address));
+        F1.write((char*)&students[i].group, sizeof(students[i].group));
+        F1.write((char*)&students[i].rate, sizeof(students[i].rate));
     }
+    F1.close();
 
-    show_data(students, size);
     return 0;
 }
